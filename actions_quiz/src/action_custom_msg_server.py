1ddb19a7510f14af05629e9b0e5a2a39500295f2
Angel Soto Pellot <asoto22@masonlive.gmu.edu>
2020-09-28 16:45:01 +0000

    Action Quiz Finished

--- actions_quiz/src/action_custom_msg_server.py
+++ actions_quiz/src/action_custom_msg_server.py
@@ -3,36 +3,40 @@ import rospy
 import actionlib
 # from actionlib.msg import TestActionFeedback, TestActionResult, TestAction
 # from geometry_msgs.msg import Twist
-from actions_quiz.msg import CustomActionMsgFeedback, CustomActionMsgResult, CustomActionMsg
+from actions_quiz.msg import CustomActionMsgFeedback, CustomActionMsgResult, CustomActionMsgAction
 from std_msgs.msg import Empty
 
 class CustomActionClass(object):
-  _feedback   = TestActionFeedback()
-  _result     = TestActionResult()
+  _feedback   = CustomActionMsgFeedback()
+  _result     = CustomActionMsgResult()
   _pubTakoff  = rospy.Publisher("/drone/takeoff", Empty, queue_size=10)
   _pubLand  = rospy.Publisher("/drone/land", Empty, queue_size=10)
 
   def __init__(self):
-    self._as = actionlib.SimpleActionServer("/action_custom_msg_as", TestAction, self.goal_callback, False)
+    self._as = actionlib.SimpleActionServer("/action_custom_msg_as", CustomActionMsgAction, self.goal_callback, False)
     self._as.start()
     
   def goal_callback(self, goal):
     success = True
 
     # publish info to the console for the user
-    if (goal == 'TAKEOFF'):
+    if (goal.goal == 'TAKEOFF'):
+        rospy.loginfo("Taking off!")
         self._pubTakoff.publish(Empty())
-        _feedback.feedback = "TAKEOFF"
-    elif (goal == 'LAND'):
+        self._feedback.feedback = "TAKEOFF"
+    elif (goal.goal == 'LAND'):
+        rospy.loginfo("Landing!")
         self._pubLand.publish(Empty())
-        _feedback.feedback = "LAND"
+        self._feedback.feedback = "LAND"
     else:
         rospy.loginfo('The goal has been cancelled/preempted')
         self._as.set_preempted()
         success = False
 
+    self._as.publish_feedback(self._feedback)
+
     if success:
-        rospy.loginfo('Succeeded in creating the square')
+        rospy.loginfo('Success!')
         self._as.set_succeeded(self._feedback)
       
 if __name__ == '__main__':
